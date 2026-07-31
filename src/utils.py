44231a69c27b3ad0b4569dcba79b3429c9e0e384
Arman <armanjamwal@hotmail.com>
2026-07-31 17:31:31 +0530

    model evaluation hyperperameter tuning

--- src/utils.py
+++ src/utils.py
@@ -22,21 +22,22 @@ def save_obj(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train, y_train,X_test,y_test,models): # ,param
+def evaluate_models(X_train, y_train,X_test,y_test,models,param): # ,param
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            # para=param[list(models.keys())[i]]
+            # print(list(models.keys())[i])
+            para=param[list(models.keys())[i]]
 
-            # gs = GridSearchCV(model,para,cv=3)
-            # gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            # model.set_params(**gs.best_params_)
-            # model.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-            model.fit(X_train, y_train)  # Train model
+            # model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -45,7 +46,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models): # ,param
             train_model_score = r2_score(y_train, y_train_pred)
 
             test_model_score = r2_score(y_test, y_test_pred)
-
+            
             report[list(models.keys())[i]] = test_model_score
 
         return report
